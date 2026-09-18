fd28a06041846ddbe5f6c24399177f1dcdd3ca8d
Ayesha Javaid <ayeshajavaid010@gmail.com>
2026-09-18 14:50:09 +0500

    modified teleportation 3 qubit code

--- teleportation/teleportation.py
+++ teleportation/teleportation.py
@@ -1,15 +1,21 @@
+# Import IBM Quantum primitives
 from qiskit import QuantumCircuit
 
 # Make state
-qc = QuantumCircuit(3)
+qc = QuantumCircuit(3, 2)
+# 3 qubit and 2 classical bit
 qc.h(1)
-qc.h(2)
-qc.cx(0, 1, 2)
-
-
-
+#putting the first qubit into superposition so we can entangle it
+qc.cx(1,2)
+#entanglement
+qc.cx(0,1)
+qc.h(0)
 # Measure state
-qc.measure_all()
+qc.measure(0, 0)
+qc.measure(1, 1)
+
+
+
 
 # Draw circuit
 qc.draw("mpl")
