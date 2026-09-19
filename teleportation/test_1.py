3e42fc87b19f9c4124c2b00db69b3ab18db1dc5f
Ayesha Javaid <ayeshajavaid010@gmail.com>
2026-09-20 02:57:47 +0500

    removed gates from q0: test1

---
+++ teleportation/test_1.py
@@ -0,0 +1,23 @@
+# Import IBM Quantum primitives
+#testing when we dont apply any gate to q0 what answer it yields
+from qiskit import QuantumCircuit
+
+# Make state
+qc = QuantumCircuit(3, 2)
+# 3 qubit and 2 classical bit
+qc.h(1)
+#putting the first qubit into superposition so we can entangle it
+
+qc.cx(1,2)
+#entanglement
+# qc.cx(0,1)
+# qc.h(0)
+
+qc.measure(0, 0)
+qc.measure(1, 1)
+
+# Measure state
+
+
+# Draw circuit
+qc.draw("mpl")
